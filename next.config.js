0f637aa05838bdc82d69fddadedfa35089530f05
arrecode <axelromero99@outlook.com>
2022-06-02 23:21:45 -0300

    functional redirect

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-  images:{
-    domains: ['links.papareact.com','image.tmdb.org', 'res.cloudinary.com', 'cloudinary.com',"https://github.com/LuchoQQ","https://arrecode.com"],
-    disableStaticImages: false    
+  trailingSlash: true,
+  images: {
+    domains: ['links.papareact.com', 'image.tmdb.org', 'res.cloudinary.com', 'cloudinary.com', "https://github.com/LuchoQQ", "https://arrecode.com"],
+    disableStaticImages: false
   },
-  experimental: {
-    async redirects() {
-      return [
-        { source: '/', 
-          destination: '/?genre=fetchTrending&page=1/', 
-          permanent: true }, // a permanent redirect
-      ];
-    },
+  async redirect() {
+    return [
+      {
+        source: '/',
+        destination: '?genre=fetchTrending&page=1',
+      },
+    ];
   },
 }
 
